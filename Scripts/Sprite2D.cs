e1ee41d0b11321248b0ee625f6be413144c7e3fe
Gabriel Lins <gabrielinsmelo@gmail.com>
2023-07-22 14:29:42 -0300

    first commit: First Script

---
+++ Scripts/Sprite2D.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+
+public partial class Sprite2D : Godot.Sprite2D
+{
+  private readonly int _speed = 400;
+  private readonly float _angularSpeed = Mathf.Pi;
+
+  public Sprite2D()
+  {
+    GD.Print("Hello World!");
+  }
+
+  public override void _Process(double delta)
+  {
+    Rotation += _angularSpeed * (float)delta;
+    var velocity = Vector2.Up.Rotated(Rotation) * _speed;
+    Position += velocity * (float)delta;
+  }
+}
+
